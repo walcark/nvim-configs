8b3241cacf75b2144ebac4ec2e618aa61e86772e
walcark <kevin.walcarius@cnes.fr>
2026-06-11 21:33:38 +0200

    feat: start a new neovim config with mini.nvim as submodule.

---
+++ mini-nvim/init.lua
@@ -0,0 +1,20 @@
+-- Line numbering (relative)
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Undo change after exiting a file
+vim.opt.undofile = true
+
+-- Better split options
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Change <leader> key to Space
+vim.g.mapleader = ""
+
+-- 4 spaces instead of tabstop + equal 4 space for indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+
+
