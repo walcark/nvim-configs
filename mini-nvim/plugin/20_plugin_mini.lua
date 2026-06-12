ab509fcac66994e666b87daefc8e5b63d3a67790
walcark <kevin.walcarius@cnes.fr>
2026-06-12 14:12:58 +0200

    feat: add description of the tools required by mini.nvim.

--- mini-nvim/plugin/20_plugin_mini.lua
+++ mini-nvim/plugin/20_plugin_mini.lua
@@ -1,3 +1,14 @@
+-- Contains all imports and configuration of mini.nvim pluggins.
+-- Currently used pluggins:
+--      mini.files
+--      mini.hues
+--      mini.snippets
+--      mini.pairs
+--      mini.icons
+--      mini.pick
+
+
+-- File navigation
 require('mini.files').setup()
 vim.keymap.set('n', '<leader>e', MiniFiles.open, {desc = 'Explorer'})
 
@@ -25,4 +36,5 @@ vim.keymap.set('n', '<leader>ff', MiniPick.builtin.files, { desc = 'Find files' 
 vim.keymap.set('n', '<leader>fg', MiniPick.builtin.grep_live, { desc = 'Grep' })
 vim.keymap.set('n', '<leader>fb', MiniPick.builtin.buffers, { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>fh', MiniPick.builtin.help, { desc = 'Help' })
+
 
